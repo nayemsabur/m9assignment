836ebddb0a9739305ada18fbf48feaaf2f7325bc
nayemsabur <98073930+nayemsabur@users.noreply.github.com>
2023-10-14 01:52:27 +0600

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(ShoppingApp());
+void main() => runApp(MyApp());
+
+class Product {
+  String name;
+  String image;
+  double price;
+  int quantity;
+
+  Product({required this.name, required this.image, required this.price, this.quantity = 0});
 }
 
-class ShoppingApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ShoppingScreen(),
+      title: 'Product List Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductListScreen(),
     );
   }
 }
 
-class ShoppingItem {
-  final String name;
-  final double unitPrice;
-  int quantity;
-
-  ShoppingItem(this.name, this.unitPrice, this.quantity);
-}
-
-class ShoppingScreen extends StatefulWidget {
+class ProductListScreen extends StatefulWidget {
   @override
-  _ShoppingScreenState createState() => _ShoppingScreenState();
+  _ProductListScreenState createState() => _ProductListScreenState();
 }
 
-class _ShoppingScreenState extends State<ShoppingScreen> {
-  List<ShoppingItem> shoppingItems = [
-    ShoppingItem('Item 1', 10.0, 0),
-    ShoppingItem('Item 2', 20.0, 0),
-    ShoppingItem('Item 3', 15.0, 0),
-    ShoppingItem('Item 4', 12.0, 0),
-    ShoppingItem('Item 5', 8.0, 0),
-    ShoppingItem('Item 6', 25.0, 0),
-    ShoppingItem('Item 7', 18.0, 0),
-    ShoppingItem('Item 8', 30.0, 0),
-    ShoppingItem('Item 9', 22.0, 0),
-    ShoppingItem('Item 10', 5.0, 0),
+class _ProductListScreenState extends State<ProductListScreen> {
+  List<Product> products = [
+    Product(name: 'Product 1', image: 'Assets/images.jpeg', price: 10.0),
+    Product(name: 'Product 2', image: 'Assets/images.jpeg', price: 15.0),
+    Product(name: 'Product 3', image: 'Assets/images.jpeg', price: 20.0),
+    Product(name: 'Product 4', image: 'Assets/images.jpeg', price: 25.0),
+    Product(name: 'Product 5', image: 'Assets/images.jpeg', price: 30.0),
+    Product(name: 'Product 6', image: 'Assets/images.jpeg', price: 35.0),
   ];
 
-  void incrementItem(int index) {
-    setState(() {
-      shoppingItems[index].quantity++;
-    });
-  }
-
-  void decrementItem(int index) {
-    if (shoppingItems[index].quantity > 0) {
-      setState(() {
-        shoppingItems[index].quantity--;
-      });
-    }
-  }
-
-  double getTotalAmount() {
-    double total = 0.0;
-    for (var item in shoppingItems) {
-      total += item.quantity * item.unitPrice;
+  double calculateTotalPrice() {
+    double total = 0;
+    for (var product in products) {
+      total += product.quantity * product.price;
     }
     return total;
-  }
-
-  void showSnackbar(BuildContext context) {
-    final snackBar = SnackBar(
-      content: Text('Congratulations! Checkout Successful'),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Shopping Screen'),
+        title: Text('Product List'),
       ),
-      body: ListView.builder(
-        itemCount: shoppingItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(shoppingItems[index].name),
-            subtitle: Text('Price: \$${shoppingItems[index].unitPrice.toStringAsFixed(2)}'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.remove),
-                  onPressed: () {
-                    decrementItem(index);
-                  },
-                ),
-                Text(shoppingItems[index].quantity.toString()),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    incrementItem(index);
-                  },
-                ),
-              ],
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Image.asset(products[index].image,),
+                  title: Text(products[index].name),
+                  subtitle:Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.remove,),
+                        onPressed: () {
+                          setState(() {
+                            if (products[index].quantity > 0) {
+                              products[index].quantity--;
+                            }
+                          });
+                        },
+                      ),
+                      Text(products[index].quantity.toString()),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          setState(() {
+                            products[index].quantity++;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  trailing:  Text('\$${products[index].price.toStringAsFixed(2)}'),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Total: \$${calculateTotalPrice().toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: ElevatedButton(
+         style: ElevatedButton.styleFrom(fixedSize: Size(200, 50),backgroundColor: Colors.red),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Congratulations! Check out successful.'),
             ),
           );
         },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'Total Amount: \$${getTotalAmount().toStringAsFixed(2)}',
-            style: TextStyle(fontSize: 18),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showSnackbar(context);
-        },
-        child: Icon(Icons.check),
+        child: Text("Check"),
+
       ),
     );
   }
